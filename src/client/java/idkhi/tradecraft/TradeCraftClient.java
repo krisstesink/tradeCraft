564c74c67a93df89cbfa698130ec86909f10c5de
ktesink <krisstesink1@gmail.com>
2024-06-12 14:10:06 +0200

    add Fabric project skeleton

---
+++ src/client/java/idkhi/tradecraft/TradeCraftClient.java
@@ -0,0 +1,10 @@
+package idkhi.tradecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TradeCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/idkhi/tradecraft/TradeCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package idkhi.tradecraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TradeCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
